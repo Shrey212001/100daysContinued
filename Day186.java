eb56bd40f5b9c5ee2dcd133f94a80e776fa2e1aa
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-04-07 19:55:45 +0530

    Priyanka and Toys

---
+++ Day186.java
@@ -0,0 +1,174 @@
+/*
+
+Topic:- Priyanka and Toys
+
+Link:- https://www.hackerrank.com/challenges/priyanka-and-toys/problem?isFullScreen=true
+
+Problem:-
+
+Priyanka works for an international toy company that ships by container. Her task is to the determine the lowest cost way to combine her orders for shipping. She has a list of item weights. The shipping company has a requirement that all items loaded in a container must weigh less than or equal to 4 units plus the weight of the minimum weight item. All items meeting that requirement will be shipped in one container.
+
+What is the smallest number of containers that can be contracted to ship the items based on the given list of weights?
+
+Function Description
+
+Complete the toys function in the editor below. It should return the minimum number of containers required to ship.
+
+toys has the following parameter(s):
+
+w: an array of integers that represent the weights of each order to ship
+
+Input Format
+
+The first line contains an integer , the number of orders to ship.
+The next line contains  space-separated integers, , representing the orders in a weight array.
+
+Output Format
+
+Return the integer value of the number of containers Priyanka must contract to ship all of the toys.
+
+
+
+
+Solution:-
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+  private static Reader in;
+  private static PrintWriter out;
+
+  public static void main(String[] args) throws IOException {
+    in = new Reader();
+    out = new PrintWriter(System.out, true);
+    int N = in.nextInt();
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)arr[i] = in.nextInt();
+    Arrays.sort(arr);
+    int last = -10000;
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+      if (last+4 < arr[i]) {
+        last = arr[i];
+        count++;
+      }
+    }
+    out.println(count);
+    out.close();
+    System.exit(0);
+  }
+
+  static class Reader {
+    final private int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public Reader() {
+      din = new DataInputStream(System.in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public Reader(String file_name) throws IOException {
+      din = new DataInputStream(new FileInputStream(file_name));
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException {
+      byte[] buf = new byte[1024];
+      int cnt = 0;
+      byte c = read();
+      while (c <= ' ')
+        c = read();
+      do {
+        buf[cnt++] = c;
+      } while ((c = read()) != '\n');
+      return new String(buf, 0, cnt);
+    }
+
+    public String next() throws IOException {
+      byte[] buf = new byte[1024];
+      int cnt = 0;
+      byte c = read();
+      while (c <= ' ')
+        c = read();
+      do {
+        buf[cnt++] = c;
+      } while ((c = read()) > ' ');
+      return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ')
+        c = read();
+      boolean neg = (c == '-');
+      if (neg)
+        c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (neg)
+        return -ret;
+      return ret;
+    }
+
+    public long nextLong() throws IOException {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ')
+        c = read();
+      boolean neg = (c == '-');
+      if (neg)
+        c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (neg)
+        return -ret;
+      return ret;
+    }
+
+    public double nextDouble() throws IOException {
+      double ret = 0, div = 1;
+      byte c = read();
+      while (c <= ' ')
+        c = read();
+      boolean neg = (c == '-');
+      if (neg)
+        c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (c == '.')
+        while ((c = read()) >= '0' && c <= '9')
+          ret += (c - '0') / (div *= 10);
+      if (neg)
+        return -ret;
+      return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1)
+        buffer[0] = -1;
+    }
+
+    private byte read() throws IOException {
+      if (bufferPointer == bytesRead)
+        fillBuffer();
+      return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+      if (din == null)
+        return;
+      din.close();
+    }
+  }
+}
